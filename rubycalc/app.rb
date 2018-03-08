6466bea844732c32d24229ea6ec716da74e28f85
ngosteven <36535700+ngosteven@users.noreply.github.com>
2018-03-08 09:45:40 +0000

    first commit basic, non prompt

--- rubycalc/app.rb
+++ rubycalc/app.rb
@@ -1,28 +1,8 @@
-class Basic
+require_relative "basic.rb"
 
-	num1 = 1
-	num2 = 2
+sum1 = Basic.new 10, 2
 
-	attr_accessor :firstint, :secondint
-
-	def initialize firstint, secondint
-		self.firstint = firstint
-		self.secondint = secondint
-	end 
-
-	def cal_plus
-	  	firstint + secondint
-	end
-
-	def cal_minus
-	  	firstint - secondint
-	end
-
-	def cal_multi
-	  	firstint * secondint
-	end
-
-	def cal_div 
-	  	firstint / secondint
-	end
-end
+p sum1.cal_plus
+p sum1.cal_minus
+p sum1.cal_multi
+p sum1.cal_div
